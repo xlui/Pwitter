839de34653e1bee704c5277006d93cec7346398f
xlui <liuqi0315@gmail.com>
2019-07-06 14:49:00 +0800

    Frontend: sign up

--- web/src/signUp.js
+++ web/src/signUp.js
@@ -1,7 +1,124 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Button, Card, Col, Form, Input, Row} from "antd";
 
-export default function () {
+export default Form.create()(function (props) {
+    const [confirmDirty, setConfirmDirty] = useState(false)
+    const {getFieldDecorator} = props.form
+
+    const handleConfirmBlur = e => {
+        const {value} = e.target
+        setConfirmDirty(confirmDirty || !!value)
+    }
+    const validateToNextPassword = (rule, value, callback) => {
+        const {form} = props
+        if (value && confirmDirty) {
+            form.validateFields(['confirm'], {
+                force: true
+            })
+        }
+        callback()
+    }
+    const compareToFirstPassword = (rule, value, callback) => {
+        const {form} = props
+        if (value && value !== form.getFieldValue('password')) {
+            callback('Two passwords that you enter is inconsistent!')
+        } else {
+            callback()
+        }
+    }
+    const handleSubmit = e => {
+        e.preventDefault()
+        props.form.validateFields(((errors, values) => {
+            if (!errors) {
+                alert(JSON.stringify(values))
+            }
+        }))
+    }
+
     return (
-        <div>sign up</div>
+        <Row type="flex" justify="center" align="middle" style={{height: "100%"}}>
+            <Col span={6}>
+                <Card title="Join pwitter now!" style={{width: "auto"}}>
+                    <Form labelCol={{span: 8}} wrapperCol={{span: 16}} labelAlign="left" onSubmit={handleSubmit}>
+                        <Form.Item label="Username:">
+                            {
+                                getFieldDecorator('username', {
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: 'Please input your username!'
+                                        }
+                                    ]
+                                })(<Input/>)
+                            }
+                        </Form.Item>
+                        <Form.Item label="E-mail:">
+                            {
+                                getFieldDecorator('email', {
+                                    rules: [
+                                        {
+                                            type: 'email',
+                                            message: 'The input is not a valid E-mail!'
+                                        },
+                                        {
+                                            required: true,
+                                            message: 'Please input your E-mail!'
+                                        }
+                                    ]
+                                })(<Input/>)
+                            }
+                        </Form.Item>
+                        <Form.Item label="Password:" hasFeedback>
+                            {
+                                getFieldDecorator('password', {
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: 'Please input your password!'
+                                        },
+                                        {
+                                            validator: validateToNextPassword
+                                        }
+                                    ]
+                                })(<Input.Password/>)
+                            }
+                        </Form.Item>
+                        <Form.Item label="Confirm:" hasFeedback>
+                            {
+                                getFieldDecorator('confirm', {
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: 'Please confirm your password!'
+                                        },
+                                        {
+                                            validator: compareToFirstPassword
+                                        }
+                                    ]
+                                })(<Input.Password onBlur={handleConfirmBlur}/>)
+                            }
+                        </Form.Item>
+                        <Form.Item label="Nickname:">
+                            {
+                                getFieldDecorator('nickname', {
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: 'Please input your nickname!',
+                                            whiteSpace: true
+                                        }
+                                    ]
+                                })(<Input/>)
+                            }
+                        </Form.Item>
+                        <Form.Item wrapperCol={{span: 16, offset: 8}}>
+                            <Button type="primary" htmlType="submit">
+                                Sign up
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Card>
+            </Col>
+        </Row>
     )
-}
+})
--- web/src/welcome.js
+++ web/src/welcome.js
@@ -15,9 +15,9 @@ export default function (props) {
 
     return (
         <Row type="flex" justify="center" align="middle" style={{height: "100%"}}>
-            <Col span={5}>
+            <Col span={6}>
                 <img src={logo} width="50" height="50" alt="this is a icon"/>
-                <Card title="Find the most interesting things on the internet!" style={{width: "auto"}}>
+                <Card title="Find the most interesting things!" style={{width: "auto"}}>
                     <Button type="primary" block={true} style={{margin: "0px 5px 5px 5px"}}
                             onClick={handleSignUp}>Signup</Button>
                     <br/>
